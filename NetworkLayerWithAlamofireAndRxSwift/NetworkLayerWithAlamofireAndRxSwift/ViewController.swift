f665687b2cedf3672f48f3d457e3957c16ee934e
Grigory <grishavinyar@icloud.com>
2022-08-31 12:07:52 +0300

    Created the project and downloaded dependencies

---
+++ NetworkLayerWithAlamofireAndRxSwift/NetworkLayerWithAlamofireAndRxSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetworkLayerWithAlamofireAndRxSwift
+//
+//  Created by Григорий Виняр on 31/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.backgroundColor = .yellow
+  }
+
+
+}
+
